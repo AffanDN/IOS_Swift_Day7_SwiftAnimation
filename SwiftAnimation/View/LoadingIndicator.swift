87f432d10babe0eccb9bff7c7d6e88e8028b226b
AffanDN <affan03nusli@gmail.com>
2024-04-22 11:30:54 +0700

    Loading With Progress

---
+++ SwiftAnimation/View/LoadingIndicator.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingIndicator.swift
+//  SwiftAnimation
+//
+//  Created by Macbook Pro on 22/04/24.
+//
+
+import SwiftUI
+
+struct LoadingIndicator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingIndicator()
+}
